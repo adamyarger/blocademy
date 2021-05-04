b03d77191e40f9e40e88b96f94eeb4865747317e
Adam Yarger <yargeradam@gmail.com>
2021-05-03 19:56:40 -0700

    creates container component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from 'components/header'
+import {
+  Box
+} from '@chakra-ui/react';
+import Container from 'components/container'
 
 export default function Home() {
   return (
@@ -11,9 +14,17 @@ export default function Home() {
 
       <Header></Header>
 
-      <div>
-        Bodyy here
-      </div>
+      <Box mb={20}>
+        <Box
+          as="section"
+          pt={{ base: "10rem", md: "12rem" }}
+          pb={{ base: "0", md: "5rem" }}
+        >
+          <Container>
+            container goes here
+          </Container>
+        </Box>
+      </Box>
     </div>
   )
 }

ab10167a246251a0def7a16920685a77a2a993fc
Adam Yarger <yargeradam@gmail.com>
2021-05-04 22:59:42 -0700

    adds markdown utils

---
+++ utils/md_utils.ts
@@ -0,0 +1,43 @@
+import path from 'path'
+import fs from 'fs'
+import matter from 'gray-matter'
+
+export interface PostMetadata {
+  id: string
+  title: string
+  date: string
+}
+
+export interface PostData extends PostMetadata {
+  mdString: string
+}
+
+const postsDirectory = path.join(process.cwd(), 'post/md')
+
+export class PostUtil {
+  static getAllPostIds(): { params: { id: string } }[] {
+    const fileNames = fs.readdirSync(postsDirectory)
+    return fileNames.map((fileName) => {
+      return {
+        params: {
+          id: fileName.replace(/\.md$/, ''),
+        },
+      }
+    })
+  }
+
+  static getPostData(id: string): PostData {
+    const fullPath = path.join(postsDirectory, `${id}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    const matterResult = matter(fileContents)
+    const { content, data } = matterResult
+
+    return {
+      id,
+      title: data.title,
+      date: data.date,
+      mdString: content,
+    }
+  }
+}
